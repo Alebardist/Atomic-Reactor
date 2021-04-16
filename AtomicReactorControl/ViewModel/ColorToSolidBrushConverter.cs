881925ca49b0a502d89b1c97b62e1db6b55f7355
Michel R <zzzvezda24@gmail.com>
2021-04-16 12:10:11 +0300

    added missed ColorToSolidBrushConverter class

---
+++ AtomicReactorControl/ViewModel/ColorToSolidBrushConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AtomicReactorControl.ViewModel
+{
+    internal class ColorToSolidBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Color)value == Colors.Black)
+            {
+                return new SolidColorBrush(Colors.Black);
+            }
+            else if ((Color)value == Colors.Orange)
+            {
+                return new SolidColorBrush(Colors.Orange);
+            }
+            else if ((Color)value == Colors.Green)
+            {
+                return new SolidColorBrush(Colors.Green);
+            }
+            else throw new ArgumentException("Got Wrong color");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
