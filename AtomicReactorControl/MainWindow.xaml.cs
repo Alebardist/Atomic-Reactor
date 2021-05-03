b266e034ff44d2b0b53d2592c086365cf052d0f3
Mikel R <zzzvezda24@gmail.com>
2021-05-03 17:11:50 +0300

    Major changes: project rewrited according to mvvm pattern. UI: setted format 0.00 for numbers

--- AtomicReactorControl/MainWindow.xaml.cs
+++ AtomicReactorControl/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using AtomicReactorControl.Model;
-using AtomicReactorControl.ViewModel.Interfaces;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AtomicReactorControl
 {
@@ -9,44 +7,19 @@ namespace AtomicReactorControl
     /// </summary>
     public partial class MainWindow : Window
     {
-        /* fake initialization is necessary because
-         * wpf is trying to call each method of
-         * MainWindow before its constructor 
-         */
-        private readonly Launcher _launcher = new Launcher();
-
         public MainWindow()
         {
             InitializeComponent();
-
-            IReactorParams reactorParams = (IReactorParams)this.FindResource("ResourceReactorParams");
-            Reactor reactor = new Reactor(reactorParams);
-
-            _launcher = new Launcher(reactorParams, reactor);  // real initialization
         }
 
         private void Button_Click_Start(object sender, RoutedEventArgs e)
         {
-            _launcher.RunTaskReactorCycleAndResetToken();
-
             BTstart.IsEnabled = false;
         }
 
         private void BTreset_Click(object sender, RoutedEventArgs e)
         {
-            _launcher.CancelTaskOfReactorCycleAndResetReactorParams();
-
             BTstart.IsEnabled = true;
-        }
-
-        private void RadioButton_mod1_Checked(object sender, RoutedEventArgs e)
-        {
-            _launcher.SwitchReactorModToRealTime();
-        }
-
-        private void RadioButton_mod2_Checked(object sender, RoutedEventArgs e)
-        {
-            _launcher.SwitchReactorModToImmediate();
         }
     }
 }
---
+++ AtomicReactorControl/ViewModel/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using AtomicReactorControl.Enums;
+using AtomicReactorControl.Model;
+using AtomicReactorControl.ViewModel.Interfaces;
+using GalaSoft.MvvmLight.CommandWpf;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AtomicReactorControl.ViewModel
+{
+    internal class MainViewModel
+    {
+        public IReactorParams ParamsForReactor { get => _paramsForReactor; set => _paramsForReactor = value; }
+        public ICommand Start { get => _start; set => _start = value; }
+        public ICommand Reset { get => _reset; set => _reset = value; }
+        public ICommand SetModRealTime { get => _setModRealTime; set => _setModRealTime = value; }
+        public ICommand SetModByFormulae { get => _setModByFormulae; set => _setModByFormulae = value; }
+
+        private Reactor _reactor;
+        private IReactorParams _paramsForReactor;
+        private ICommand _start;
+        private ICommand _reset;
+        private ICommand _setModRealTime;
+        private ICommand _setModByFormulae;
+
+        // cancellation tokens
+        private static CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        private CancellationToken _token;
+
+        public MainViewModel()
+        {
+            ParamsForReactor = new ParamsForReactor();
+            _reactor = new Reactor(ParamsForReactor);
+            _token = _cancellationTokenSource.Token;
+
+            _start = new RelayCommand(StartCycle);
+            _reset = new RelayCommand(ResetParamsAndToken);
+            SetModRealTime = new RelayCommand(SwitchReactorModToRealTime);
+            SetModByFormulae = new RelayCommand(SwitchReactorModToImmediate);
+        }
+
+        public void SwitchReactorModToRealTime()
+        {
+            if (_paramsForReactor != null)
+            {
+                _paramsForReactor.CurrentWorkMode = WorkMode.HeatWithinWork;
+            }
+        }
+
+        public void SwitchReactorModToImmediate()
+        {
+            if (_paramsForReactor != null)
+            {
+                _paramsForReactor.CurrentWorkMode = WorkMode.HeatByFormulae;
+            }
+        }
+
+        private void StartCycle()
+        {
+            new Task(() => _reactor.ReactorCycle(_token)).Start();
+        }
+
+        private void ResetParamsAndToken()
+        {
+            _cancellationTokenSource.Cancel();
+
+            _paramsForReactor.ResetParams();
+            ResetToken();
+        }
+
+        private void ResetToken()
+        {
+            if (_cancellationTokenSource == null || _cancellationTokenSource.IsCancellationRequested)
+            {
+                _cancellationTokenSource = new CancellationTokenSource();
+                _token = _cancellationTokenSource.Token;
+            }
+        }
+    }
+}
